dd23b9c0ee49f1025cebd5c5040fbc26177dbb7a
vermarenu349 <vermarenu349@gmail.com>
2024-05-29 18:31:47 +0530

    init prject

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { middleware } from "./middleware";
+
+const app = express();
+
+app.use(express.json());
+app.use(middleware);
+app.get("/user", (req, res) => {
+    res.send({
+        name: "John Doe",
+        age: 25,
+    });
+});
+
+app.post("/user", (req, res) => {
+    const user = req.body;
+    res.send({
+        ...user,
+        id: 1,
+    });
+});
+
+app.listen(3000);
